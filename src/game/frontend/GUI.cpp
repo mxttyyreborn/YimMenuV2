9721bf26c1fce44e2f862abb15ec0f81bb14d699
mxttyyreborn <50087730+mxttyyreborn@users.noreply.github.com>
2025-12-18 23:11:09 +0000

    Update GUI.cpp
    Changed "INSERT" keybind to "DELETE" keybind due to some keyboard not having an INSERT key.

--- src/game/frontend/GUI.cpp
+++ src/game/frontend/GUI.cpp
@@ -63,7 +63,7 @@ namespace YimMenu
 	void GUI::WndProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	{
 		if (msg == WM_KEYUP 
-			&& (wparam == VK_INSERT || (wparam == VK_OEM_5 && (GetKeyState(VK_CONTROL) & 0x8000) != 0)))
+			&& (wparam == VK_DELETE || (wparam == VK_OEM_5 && (GetKeyState(VK_CONTROL) & 0x8000) != 0)))
 		{
 			// Persist and restore the cursor position between menu instances
 			static POINT CursorCoords{};
