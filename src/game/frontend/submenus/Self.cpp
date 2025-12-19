f67ba8446baddf8fa390f2959fc26b178c82df5a
mxttyyreborn <50087730+mxttyyreborn@users.noreply.github.com>
2025-12-19 23:10:44 +0000

    Add 'opengunvan' option to globals group

--- src/game/frontend/submenus/Self.cpp
+++ src/game/frontend/submenus/Self.cpp
@@ -29,6 +29,7 @@ namespace YimMenu::Submenus
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("mobileradio"_J));
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("keepplayerclean"_J));
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("disablecriticalhits"_J));
+		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("opengunvan"_J));
 
 		toolsGroup->AddItem(std::make_shared<CommandItem>("skipcutscene"_J));
 		toolsGroup->AddItem(std::make_shared<CommandItem>("skipconversation"_J));
