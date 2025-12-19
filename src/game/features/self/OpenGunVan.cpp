246484b527e11998fe21206bdad8b0fbad154ac5
mxttyyreborn <50087730+mxttyyreborn@users.noreply.github.com>
2025-12-19 23:17:56 +0000

    Rename OnExecute to OnCall and update logic
    Refactor OpenGunVan command execution method and adjust script loading logic.

--- src/game/features/self/OpenGunVan.cpp
+++ src/game/features/self/OpenGunVan.cpp
@@ -1,27 +1,16 @@
 #include "core/commands/Command.hpp"
 #include "game/gta/Natives.hpp"
-#include "game/backend/Self.hpp"
 
-namespace YimMenu::Features
+namespace YimMenu::Features::Self
 {
 	class OpenGunVan final : public Command
 	{
 		using Command::Command;
 
-		void OnExecute() override
+		void OnCall() override
 		{
-			const Hash gunVanScript = MISC::GET_HASH_KEY("gunvan_shop");
-
-			if (!SCRIPT::HAS_SCRIPT_LOADED(gunVanScript))
-			{
-				SCRIPT::REQUEST_SCRIPT(gunVanScript);
-				while (!SCRIPT::HAS_SCRIPT_LOADED(gunVanScript))
-					WAIT(0);
-
-				SCRIPT::START_NEW_SCRIPT(gunVanScript, 0);
-			}
-
 			HUD::SET_FRONTEND_ACTIVE(true);
+
 			HUD::ACTIVATE_FRONTEND_MENU(
 				MISC::GET_HASH_KEY("FE_MENU_VERSION_GUNVAN"),
 				false,
