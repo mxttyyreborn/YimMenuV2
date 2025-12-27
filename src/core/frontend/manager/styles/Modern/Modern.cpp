043552d4a447f01c81d0564da165699f87ee1ff1
mxttyyreborn <50087730+mxttyyreborn@users.noreply.github.com>
2025-12-27 19:33:10 +0000

    Update Modern.cpp

--- src/core/frontend/manager/styles/Modern/Modern.cpp
+++ src/core/frontend/manager/styles/Modern/Modern.cpp
@@ -143,7 +143,7 @@ namespace YimMenu
 				{
 					ImFont* optionsFont = YimMenu::UIManager::GetOptionsFont();
 					if (optionsFont)
-						ImGui::PushFont(optionsFont);
+						ImGui::PushFont(optionsFont,YimMenu::Menu::Font::g_OptionsFontSize);
 
 					activeSubmenu->Draw();
 
