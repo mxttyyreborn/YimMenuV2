34d8195b2348fff34de24062caed99921544d7d4
mxttyyreborn <50087730+mxttyyreborn@users.noreply.github.com>
2025-12-27 19:35:23 +0000

    Update Menu.cpp

--- src/game/frontend/Menu.cpp
+++ src/game/frontend/Menu.cpp
@@ -91,37 +91,18 @@ namespace YimMenu
 		style.Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.47f, 0.75f, 0.47f, 1.00f);
 
 		style.GrabRounding = style.FrameRounding = style.ChildRounding = style.WindowRounding = 8.0f;
-	}
-
-	static const ImWchar* GetGlyphRangesCyrillicOnly()
-	{
-		static const ImWchar ranges[] =
-		    {
-		        0x0400,
-		        0x052F, // Cyrillic + Cyrillic Supplement
-		        0x2DE0,
-		        0x2DFF, // Cyrillic Extended-A
-		        0xA640,
-		        0xA69F, // Cyrillic Extended-B
-		        0,
-		    };
-		return &ranges[0];
-	}
+	}	
 
 	static ImFont* CreateFontWithCyrillicSupport(ImGuiIO& io, float size)
 	{
 		ImFontConfig FontCfg{};
 		FontCfg.FontDataOwnedByAtlas = false;
-
-		auto font = io.Fonts->AddFontFromMemoryTTF(const_cast<std::uint8_t*>(Fonts::MainFont), sizeof(Fonts::MainFont), size, &FontCfg, io.Fonts->GetGlyphRangesDefault());
-
-		// just use Arial for Cyrillic
+		auto font = io.Fonts->AddFontFromMemoryTTF(const_cast<std::uint8_t*>(Fonts::MainFont), sizeof(Fonts::MainFont), size, &FontCfg);
 
 		FontCfg.MergeMode = true;
-		io.Fonts->AddFontFromFileTTF((std::filesystem::path(std::getenv("SYSTEMROOT")) / "Fonts" / "arial.ttf").string().c_str(), size, &FontCfg, GetGlyphRangesCyrillicOnly());
-
+		// just use Arial for Cyrillic
+		io.Fonts->AddFontFromFileTTF((std::filesystem::path(std::getenv("SYSTEMROOT")) / "Fonts" / "arial.ttf").string().c_str(), size, &FontCfg);		
 		io.Fonts->Build();
-
 		return font;
 	}
 
@@ -134,17 +115,17 @@ namespace YimMenu
 		IO.LogFilename = NULL;
 		ImFontConfig FontCfg{};
 		FontCfg.FontDataOwnedByAtlas = false;
-
 		IO.Fonts->Clear();
-		Menu::Font::g_DefaultFont = CreateFontWithCyrillicSupport(IO, Menu::Font::g_DefaultFontSize);
-		Menu::Font::g_OptionsFont = CreateFontWithCyrillicSupport(IO, Menu::Font::g_OptionsFontSize);
-		Menu::Font::g_ChildTitleFont = CreateFontWithCyrillicSupport(IO, Menu::Font::g_ChildTitleFontSize);
-		Menu::Font::g_ChatFont = CreateFontWithCyrillicSupport(IO, Menu::Font::g_ChatFontSize);
-		Menu::Font::g_OverlayFont = CreateFontWithCyrillicSupport(IO, Menu::Font::g_OverlayFontSize);
-		static const ImWchar full_range[] = {0x0020, 0xFFFF, 0};
-		Menu::Font::g_AwesomeFont = IO.Fonts->AddFontFromMemoryTTF(const_cast<std::uint8_t*>(Fonts::IconFont), sizeof(Fonts::IconFont), Menu::Font::g_AwesomeFontSize, &FontCfg, full_range);
 
-		UIManager::SetOptionsFont(Menu::Font::g_OptionsFont);
+		//Starting from version 1.92, it comes with the latest backend and does not require a specified font range.
+		Menu::Font::g_DefaultFont = CreateFontWithCyrillicSupport(IO, Menu::Font::g_DefaultFontSize);
+
+		FontCfg.MergeMode = true;
+		Menu::Font::g_AwesomeFont = IO.Fonts->AddFontFromMemoryTTF(const_cast<std::uint8_t*>(Fonts::IconFont), sizeof(Fonts::IconFont), Menu::Font::g_AwesomeFontSize, &FontCfg);		
+
+		if (!IO.Fonts->IsBuilt())
+			IO.Fonts->Build();
+		UIManager::SetOptionsFont(Menu::Font::g_DefaultFont);
 		Renderer::SetFontsUpdated();
 	}
 }
